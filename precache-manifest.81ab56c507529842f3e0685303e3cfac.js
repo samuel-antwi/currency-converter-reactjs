7ed46c35399151337bc7f5db09f0ed754363fb8b
Samuel Antwi <antwisam80@yahoo.com>
2020-04-25 14:55:28 +0100

    Updates

---
+++ precache-manifest.81ab56c507529842f3e0685303e3cfac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ade20434c9da42d7bc835a63539fef2d",
+    "url": "/samuel-antwi/currency-converter-reactjst/index.html"
+  },
+  {
+    "revision": "7b2e1e8025ee43320a61",
+    "url": "/samuel-antwi/currency-converter-reactjst/static/css/main.fbb38dc2.chunk.css"
+  },
+  {
+    "revision": "38135e04a6aa3cf580b7",
+    "url": "/samuel-antwi/currency-converter-reactjst/static/js/2.cc950b21.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/samuel-antwi/currency-converter-reactjst/static/js/2.cc950b21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b2e1e8025ee43320a61",
+    "url": "/samuel-antwi/currency-converter-reactjst/static/js/main.b651bcfa.chunk.js"
+  },
+  {
+    "revision": "c6ef62b92cc54bf73f93",
+    "url": "/samuel-antwi/currency-converter-reactjst/static/js/runtime-main.3ffbb668.js"
+  },
+  {
+    "revision": "078aa3f09332c2484dbfd7239ef39e2d",
+    "url": "/samuel-antwi/currency-converter-reactjst/static/media/loading.078aa3f0.gif"
+  },
+  {
+    "revision": "315964736bc3aa7839c1cd4d9f712697",
+    "url": "/samuel-antwi/currency-converter-reactjst/static/media/pound.31596473.jpg"
+  }
+]);
